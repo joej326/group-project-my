d0ab9bf5918d7c788dd1b3186d37af60363b0d5c
dontimoteo21 <timothyjames.andrus@gmail.com>
2017-06-26 19:09:54 -0600

    "fixed route to home"

--- the-project/src/routes.js
+++ the-project/src/routes.js
@@ -5,7 +5,7 @@ import Home from './components/AuthComp/Home/Home';
 import Callback from './components/AuthComp/Callback/Callback';
 import Auth from './components/AuthComp/Auth/Auth';
 import history from './history';
-import ExecutiveComp from './components/ExecutiveComp/executiveView';
+
 
 // *** BLENDING 
 import { Provider } from 'react-redux';
@@ -30,9 +30,9 @@ export const makeMainRoutes = () => {
      <Provider store={createStoreWithMiddleware(reducers)}>
       <BrowserRouter history={history} component={AuthApp}>
         <div>
-          <Route path="/" render={(props) => <AuthApp auth={auth} {...props} />} />
-          <Route path="/home" render={(props) => <Home auth={auth} {...props} />} />
-          <Route path="/callback" render={(props) => {
+          <Route exact path="/" render={(props) => <AuthApp auth={auth} {...props} />} />
+          <Route exact path="/home" render={(props) => <Home auth={auth} {...props} />} />
+          <Route exact path="/callback" render={(props) => {
             handleAuthentication(props);
             return <Callback {...props} /> 
           }}/>
