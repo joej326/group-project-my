01f57c90282401095a85604e205c5db75fbc08c0
Joseph Joyce <joej326@gmail.com>
2017-06-28 14:15:53 -0600

    awdawd

---
+++ the-project/src/containers/Taskboard_App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+
+import Taskboard from './TaskBoard';
+
+export default class TaskBoardApp extends Component {
+  constructor(props){
+    super(props);
+
+
+  }
+
+
+  render(){
+
+
+    return (
+      <div className='taskboard-app'>
+        <h1 className='taskboard-heading'>TASKBOARD</h1>
+        <Taskboard />
+      </div>
+    )
+  }
+}
+
+// function mapStateToProps(state){
+//   return {
+//     toDoItem: toDoItem
+//   }
+// }
+//
+// export default connect(mapStateToProps,{})(ToDoContainer)
