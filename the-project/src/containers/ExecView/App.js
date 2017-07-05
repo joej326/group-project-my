44c4915d813050cc5428f3285311dc33e11f9cee
actualChandler <chambler97@gmail.com>
2017-07-05 17:29:36 -0600

    succesfully pulling data from charts in db

--- the-project/src/containers/ExecView/App.js
+++ the-project/src/containers/ExecView/App.js
@@ -2,13 +2,71 @@ import React, { Component } from 'react'
 
 import { connect } from 'react-redux'
 
-export default class ExecView extends Component {
+// *** Chart Actions
+import { fetchRevenue } from '../../actions'
+import { fetchProfitMargin } from '../../actions'
+import { fetchBudget } from '../../actions'
+import { fetchNetExpenses} from '../../actions'
+
+
+class ExecView extends Component {
+   componentDidMount(){
+      console.log("Revenue: ", this.props.fetchRevenue())
+      console.log("Profit Margin: ", this.props.fetchProfitMargin())
+      console.log("Budget: ", this.props.fetchBudget())
+      console.log("Net Expenses: ", this.props.fetchNetExpenses())
+
+   }
 
    render(){
       return (
          <div>
-         This is the ExecView!
+         
+         <div className="row">
+         <div
+         className="col-md-offset-2 col-md-4">
+            Name: Revenue
+            <br />
+            Data: Total Revenue
+            <br />
+            Chart Type: Gauge
+         </div>
+
+         <div
+         className="col-md-offset-1 col-md-4">
+            Name: Profit Margin
+            <br />
+            Data: Total Profit Margin
+            <br />
+            Chart Type: Gauge
+         </div>
+         </div>
+
+         <div className="row">
+         <div
+         className="col-md-offset-2 col-md-4">
+            Name: Budget
+            <br />
+            Data: Total Budget
+            <br />
+            Chart Type: Gauge
+         </div>
+
+         <div
+         className="col-md-offset-1 col-md-4">
+            Name: net Expenses
+            <br />
+            Data: Net Expenses Total
+            <br />
+            Chart Type: Gauge
+         </div>
+         </div>
+
+
+
          </div>
       )
    }
 }
+
+export default connect(null, { fetchRevenue, fetchProfitMargin, fetchBudget, fetchNetExpenses })(ExecView)
--- the-project/src/containers/SalesView/App.js
+++ the-project/src/containers/SalesView/App.js
@@ -2,13 +2,74 @@ import React, { Component } from 'react'
 
 import { connect } from 'react-redux'
 
-export default class SalesView extends Component {
+// *** Actions
+import { fetchSalesOpps } from '../../actions'
+import { fetchRepsQ2 } from '../../actions'
+
+
+class SalesView extends Component {
+   componentDidMount(){
+      console.log("Sales Opps: ", this.props.fetchSalesOpps())
+      console.log("Reps Q2: ", this.props.fetchRepsQ2())
+
+   }
 
    render(){
       return (
          <div>
-         This is the SalesView!
+         
+         <div className="row">
+         <div
+         className="col-md-offset-1 col-md-3">
+            Name: Revenue
+            <br />
+            Data: Revenue
+            <br />
+            Chart Type: Pie
+         </div>
+
+         <div
+         className="col-md-offset-1 col-md-3">
+            Name: Net Expenses
+            <br />
+            Data: Net Expenses Total
+            <br />
+            Chart Type: Pie
+         </div>
+
+         <div
+         className="col-md-offset-1 col-md-3">
+            Name: Budget
+            <br />
+            Data: Budget Total
+            <br />
+            Chart Type: Pie
+         </div>
+         </div>
+
+         <div className="row">
+         <div
+         className="col-md-offset-2 col-md-4">
+            Name: Debt to Equity
+            <br />
+            Data: DTE
+            <br />
+            Chart Type: Spike Chart
+         </div>
+
+         <div
+         className="col-md-offset-1 col-md-4">
+            Name: Profit Margin & Net Expenses
+            <br />
+            Data: Profit Margin / Net Expenses
+            <br />
+            Chart Type: Stacked SparkLine
+         </div>
+         </div>
+
          </div>
       )
    }
 }
+
+export default connect(null, { fetchSalesOpps, fetchRepsQ2 })(SalesView)
