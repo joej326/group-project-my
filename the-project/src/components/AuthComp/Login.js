e484860ac587f18a7839013774bc4de609a849a3
dontimoteo21 <timothyjames.andrus@gmail.com>
2017-07-06 13:13:26 -0600

    flogin fixed

--- the-project/src/components/AuthComp/Login.js
+++ the-project/src/components/AuthComp/Login.js
@@ -2,47 +2,48 @@ import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
 import './Auth.css';
 import logo from './Home/react3.jpg';
-
-import { Redirect } from 'react-router-dom';
+//import { Link } from 'react-router';
+import { Route, Redirect } from 'react-router';
+//import { browserHistory } from 'react-router'
 
 class App extends Component {
   goTo(route) {
     this.props.history.replace(`/${route}`)
   }
-  login() {
-    this.props.auth.login();
-  }
+//   login() {
+//     this.props.auth.login();
+//   }
 
-  logout() {
-    this.props.auth.logout();
-  }
+//   logout() {
+//     this.props.auth.logout();
+//   }
 
-  render() {
-    const { isAuthenticated } = this.props.auth;
-console.log('login.js ', isAuthenticated())
+//   render() {
+//     const { isAuthenticated } = this.props.auth;
+// console.log('login.js ', isAuthenticated())
+// handleClick = () => {
+//   this.router.push('/exec')
+// };
+render() {
     return (
        <div className="container-fluid">
        
             
-            {
-              !isAuthenticated() && (
+            
+               
                   <div className="contt">
                   
                   <img src={logo} alt="My logo" />
                   <h5>Admin-Redux</h5>
-                   <Button 
-                  className="button button1"
-                  onClick={this.login.bind(this)}>
+                  <a href="/exec"><Button 
+                  className="button button1">
                   Sign In
-                  </Button>
+                  </Button></a>
                   </div>
-                )
-            }
-            {
-              isAuthenticated() && (
-                  <Redirect to="/"/>
-                )
-            }
+                
+            
+                  
+            
          
       </div>
     );
