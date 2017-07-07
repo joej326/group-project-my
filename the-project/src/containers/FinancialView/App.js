80464249877ab96484d889a73f8e8f5835666a02
Joseph Joyce <joej326@gmail.com>
2017-07-07 07:53:49 -0600

    fixed sales

--- the-project/src/containers/FinancialView/App.js
+++ the-project/src/containers/FinancialView/App.js
@@ -337,104 +337,11 @@ class FinancialView extends Component {
          <div className='chart debt-to-equity-div'>
            <canvas id="debt-to-equity" width="5vw" height="5vw"></canvas>
 
-
-         <div className="row some-space">
-         </div>
-
-         <div className="row">
-         <div
-         className="col-md-offset-2 col-md-8">
-
-            <div className="panel panel-default">
-              <div className="panel-heading">Sales Opporitunities</div>
-              <table
-              className="table-hover black-text"
-              style= {
-                 {
-                  width: '100%',
-                 marginTop: '10px',
-                 marginLeft: '10px',
-                 marginBottom: '10px'
-                 }
-              }>
-                <tr>
-                   <th>Name</th>
-                   <th>Stage</th>
-                   <th>Value</th>
-                  </tr>
-               <tr>
-                   <td>Maximun HP</td>
-                   <td>Qualified</td>
-                   <td>34980</td>
-               </tr>
-               <tr>
-                   <td>Universal N.LLC</td>
-                   <td>Executive Sponsor</td>
-                   <td>45790</td>
-               </tr>
-               <tr>
-                   <td>CytoSport</td>
-                   <td>Discovery</td>
-                   <td>58230</td>
-               </tr>
-               <tr>
-                   <td>Gaspari</td>
-                   <td>Qualified</td>
-                   <td>89760</td>
-               </tr>
-               <tr>
-                   <td>Nutrex</td>
-                   <td>Contracts</td>
-                   <td>25650</td>
-               </tr>
-               <tr>
-                   <td>ProSource</td>
-                   <td>Discover</td>
-                   <td>41100</td>
-               </tr>
-               <tr>
-                   <td>MTech</td>
-                   <td>Executive Sponsor</td>
-                   <td>66340</td>
-               </tr>
-               <tr>
-                   <td>Optimum</td>
-                   <td>Contracts</td>
-                   <td>22900</td>
-               </tr>
-               <tr>
-                   <td>BSN</td>
-                   <td>Discovery</td>
-                   <td>89780</td>
-               </tr>
-               <tr>
-                   <td>BioQuest</td>
-                   <td>Qualified</td>
-                   <td>55900</td>
-               </tr>
-              </table>
-            </div>
-
-         </div>
-         </div>
-
-         <div className="row">
-         <div
-         className="col-md-offset-2 col-md-8">
-            Name: Reps
-            <br />
-            Data: ??
-            <br />
-            Chart Type: Bar Chart
-         </div>
-
-         </div>
-     </div>
-
          </div>
 
 
        </div>
+     </div>
       )
    }
 }
--- the-project/src/containers/SalesView/App.js
+++ the-project/src/containers/SalesView/App.js
@@ -17,47 +17,123 @@ import Chart from 'chart.js';
 
 
 class SalesView extends Component {
-   constructor(props){
-      super(props)
-      this.state = {
-         oppsData: null
-      }
-   }
-   componentDidMount(){
-         this.props.fetchSalesOpps().then((results)=>{
-               this.setState({
-            oppsData: results.payload.data
-         })
-      })
-
-
-   }
+  //  constructor(props){
+  //     super(props)
+  //     this.state = {
+  //        oppsData: null
+  //     }
+  //  }
+  //  componentDidMount(){
+  //        this.props.fetchSalesOpps().then((results)=>{
+  //              this.setState({
+  //           oppsData: results.payload.data
+  //        })
+  //     })
+   //
+   //
+  //  }
 
    render(){
       return (
          <div>
 
-         <div className="row">
-         <div
-         className="col-md-offset-2 col-md-4">
-            Name: Sales Ops
-            <br />
-            Data: ??
-            <br />
-            Chart Type: Table
-         </div>
+           <div className="row some-space">
+           </div>
 
-         <div
-         className="col-md-offset-1 col-md-4">
-            Name: Reps
-            <br />
-            Data: ??
-            <br />
-            Chart Type: Bar Chart
-         </div>
-         </div>
+           <div className="row">
+           <div
+           className="col-md-offset-2 col-md-8">
 
-         </div>
+              <div className="panel panel-default">
+                <div className="panel-heading">Sales Opporitunities</div>
+                <table
+                className="table-hover black-text"
+                style= {
+                   {
+                    width: '100%',
+                   marginTop: '10px',
+                   marginLeft: '10px',
+                   marginBottom: '10px'
+                   }
+                }>
+                  <tr>
+                     <th>Name</th>
+                     <th>Stage</th>
+                     <th>Value</th>
+                    </tr>
+                 <tr>
+                     <td>Maximun HP</td>
+                     <td>Qualified</td>
+                     <td>34980</td>
+                 </tr>
+                 <tr>
+                     <td>Universal N.LLC</td>
+                     <td>Executive Sponsor</td>
+                     <td>45790</td>
+                 </tr>
+                 <tr>
+                     <td>CytoSport</td>
+                     <td>Discovery</td>
+                     <td>58230</td>
+                 </tr>
+                 <tr>
+                     <td>Gaspari</td>
+                     <td>Qualified</td>
+                     <td>89760</td>
+                 </tr>
+                 <tr>
+                     <td>Nutrex</td>
+                     <td>Contracts</td>
+                     <td>25650</td>
+                 </tr>
+                 <tr>
+                     <td>ProSource</td>
+                     <td>Discover</td>
+                     <td>41100</td>
+                 </tr>
+                 <tr>
+                     <td>MTech</td>
+                     <td>Executive Sponsor</td>
+                     <td>66340</td>
+                 </tr>
+                 <tr>
+                     <td>Optimum</td>
+                     <td>Contracts</td>
+                     <td>22900</td>
+                 </tr>
+                 <tr>
+                     <td>BSN</td>
+                     <td>Discovery</td>
+                     <td>89780</td>
+                 </tr>
+                 <tr>
+                     <td>BioQuest</td>
+                     <td>Qualified</td>
+                     <td>55900</td>
+                 </tr>
+                </table>
+              </div>
+
+           </div>
+           </div>
+
+{/*
+//ADD BAR CHARTS HERE */}
+           {/* <div className="row">
+           <div
+           className="col-md-offset-2 col-md-8">
+              Name: Reps
+              <br />
+              Data: ??
+              <br />
+              Chart Type: Bar Chart
+           </div>
+
+           </div> */}
+       </div>
+
+
+
       )
    }
 }
