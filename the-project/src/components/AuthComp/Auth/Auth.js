968f05bb2492b40ef7e69c8af45804c409e45491
Joseph Joyce <joej326@gmail.com>
2017-07-12 15:55:48 -0600

    awdw

--- the-project/src/components/AuthComp/Auth/Auth.js
+++ the-project/src/components/AuthComp/Auth/Auth.js
@@ -1,6 +1,6 @@
 import history from '../../../history';
 import auth0 from 'auth0-js';
-import { AUTH_CONFIG } from './auth0-variables';
+
 
 export default class Auth {
   auth0 = new auth0.WebAuth({
@@ -59,7 +59,7 @@ export default class Auth {
   }
 
   isAuthenticated() {
-    // Check whether the current time is past the 
+    // Check whether the current time is past the
     // access token's expiry time
     let expiresAt = JSON.parse(localStorage.getItem('expires_at'));
     return new Date().getTime() < expiresAt;
